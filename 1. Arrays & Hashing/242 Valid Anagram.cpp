ce8b0dca99b1c1baf7c7bfa6f626f0503ce63f7f
Jain1shh <jainishprajapati98@gmail.com>
2025-08-04 11:18:44 +0530

    added more problem solutions

---
+++ 1. Arrays & Hashing/242 Valid Anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }else{
+            unordered_map<char, int> mpp1;
+            unordered_map<char, int> mpp2;
+
+            for(auto it : s){
+                mpp1[it]++;
+            }
+
+            for(auto it : t){
+                mpp2[it]++;
+            }
+
+            if(mpp1 == mpp2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
